ebedacc0c73b4d2b6e2c358171829d8fc74af652
KevinIssa <kevin.issa@ulb.be>
2022-10-21 15:29:04 +0200

    select delete presque fini

--- base_etudiants/base_etudiants/query.cpp
+++ base_etudiants/base_etudiants/query.cpp
@@ -19,13 +19,11 @@ void insert(student_t* student, database_t* data_base){
     db_add(data_base,*student); 
     cout<<"student inserted with success"<<endl;
 }
-tuple<bool,string>data_analyse(string data_filter,string filter_asked){
+bool data_analyse(string data_filter,string filter_asked){
   /**
    * function:
    */
-
     bool is_data_find=false;
-    unsigned short int vector_index=0;
     string id="id";
     string fname="fname";
     string lname="lname";
@@ -33,39 +31,118 @@ tuple<bool,string>data_analyse(string data_filter,string filter_asked){
     string birthdate="birthdate";
     vector<string>student_data={id,fname,lname,section,birthdate};//creation of a list to store the data
 
-    while(vector_index<student_data.size() or is_data_find == true){
+    while(unsigned short int vector_index=0<student_data.size() or is_data_find == true){
       if(data_filter == student_data[vector_index]){
         is_data_find=true;
       }
       vector_index++;
     }
-    if(is_data_find ==false){
-      return {is_data_find,""};
-    }
-    return {is_data_find,student_data[vector_index]};
+    return is_data_find;
 }
 
 vector<student_t*> select(string data_type,string filter_asked,student_t* student,database_t* data_base){
   /**
    * function: return a list of student sorted by the filter data_type that the user sent
+   * 
+   * il faut trouver cmt modifier les types et cmt filtrer la date de naissance et c bon(c est tres moche mais ca devrait marcher
+   * on modifiera apres)
    */
 
   vector<student_t*>sort_student_list;
   //we get the info of the data_research function
-  bool is_data_valid=get<0>(data_analyse(data_type,filter_asked));
-  string data_write=get<1>(data_analyse(data_type,filter_asked));
+  bool is_data_valid=data_analyse(data_type,filter_asked);
   if(is_data_valid==true){
     for(unsigned long int i=0;i<data_base->lsize;i++){
-      if(data_base->data[i]->id)
-        sort_student_list.push_back(student);
+      student_t* data_base_student=data_base->data[i];//doit voir si c est student_t ou student_t*
+
+      if(data_type=="id"){
+        if(data_base->data[i].id == filter_asked){//probleme de conversion 
+          sort_student_list.push_back(data_base_student);
+        }
+      }  
+
+      else if(data_type=="fname"){
+        if(data_base->data[i].fname == filter_asked){
+          sort_student_list.push_back(data_base_student);
+        }
+      }
+      
+      else if(data_type=="lname"){
+        if(data_base->data[i].lname == filter_asked){
+          sort_student_list.push_back(data_base_student);
+        }
+      }
+
+      else if(data_type=="section"){
+        if(data_base->data[i].section == filter_asked){
+          sort_student_list.push_back(data_base_student);
+        }
+      }
+
+      else if(data_type=="birthdate"){
+        if(data_base->data[i].birthdate == filter_asked){
+          sort_student_list.push_back(data_base_student);
+        }
+      }
+
     }
-    return sort_student_list;
   }
   else{
     throw invalid_argument("The data that you want to select doesn't exist.");
   }
+  return sort_student_list;
 }
 
+void delete_function(string data_type,string filter_asked,student_t* student,database_t* data_base){
+/**
+ * @brief Remove all the students in the data base that match the filter data_type =filter asked
+ * 
+ * @param
+ * 
+ * probleme: je sais pas cmt remove ds la data base sinon c les memes probs que select 
+ */
+  bool is_data_valid=data_analyse(data_type,filter_asked);
+  if(is_data_valid==true){
+    for(unsigned long int i=0;i<data_base->lsize;i++){
+      student_t* data_base_student=data_base->data[i];//doit voir si c est student_t ou student_t*
+
+      if(data_type=="id"){
+        if(data_base->data[i].id == filter_asked){//probleme de conversion 
+          data_base->data.remove(data_base_student);
+        }
+      }  
+
+      else if(data_type=="fname"){
+        if(data_base->data[i].fname == filter_asked){
+        }
+      }
+      
+      else if(data_type=="lname"){
+        if(data_base->data[i].lname == filter_asked){
+        }
+      }
+
+      else if(data_type=="section"){
+        if(data_base->data[i].section == filter_asked){
+        }
+      }
+
+      else if(data_type=="birthdate"){
+        if(data_base->data[i].birthdate == filter_asked){
+        }
+      }
+    }
+  }
+  else{
+    throw invalid_argument("The data that you want to delete doesn't exist.");
+  }
+}
+
+void update(string data_type,string filter_asked,string new_data_type,string new_filter,
+            student_t* student,database_t* data_base){
+
+
+}
 void query_result_init(query_result_t* result, const char* query) {
   struct timespec now;
   clock_gettime(CLOCK_REALTIME, &now);
