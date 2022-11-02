0e1a1631e6aaccf9adf2f815882af68736386c90
Astramast <azerqsdfazerqsdg@gmail.com>
2022-11-02 15:44:32 +0100

    pipes done

--- base_etudiants/base_etudiants/main.cpp
+++ base_etudiants/base_etudiants/main.cpp
@@ -11,14 +11,31 @@ int main(int argc, char const *argv[]) {
     db_init(&db);
     db_load(&db, db_path);
 	int sons[4];
-	//int pipes[8];
-	int pid = 1;
+	int insert_pipe_father[2];
+	int insert_pipe_son[2];
+	int select_pipe_father[2];
+	int select_pipe_son[2];
+	int delete_pipe_father[2];
+	int delete_pipe_son[2];
+	int update_pipe_father[2];
+	int update_pipe_son[2];
+	int* pipes[8] = {insert_pipe_father, insert_pipe_son, select_pipe_father, select_pipe_son, delete_pipe_father, delete_pipe_son, update_pipe_father, update_pipe_son};
+	for (int i=0; i<8; i++){
+		pipe(pipes[i]);
+	}
+	int pid;
 	for (int i=0; i<4; i++){
 		pid = fork();
 		if (pid !=0){
 			sons[i]=pid;
+			close(pipes[2*i][0];
+			close(pipes[2*i+1][1];
 		}
 		else{
+			close(pipes[2*i][1]);
+			close(pipes[2*i+1][0]);
+			int my_read = pipes[2*i][0];
+			int my_write = pipes[2*i+1][1];
 			i=4;
 		}
 	}
