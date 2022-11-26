93606d9360307c0f6d3ca792545402a504a22dd4
KevinIssa <kevin.issa@ulb.be>
2022-11-26 18:15:49 +0100

    1ere partie du serveur

--- base_etudiants/base_etudiants/utils.cpp
+++ base_etudiants/base_etudiants/utils.cpp
@@ -23,6 +23,27 @@ size_t safe_write(int fd, const void* buffer, size_t nbytes) {
     return (size_t)bytes_written;
 }
 
+int _checked(int ret, char* calling_function) {
+  if (ret < 0) {
+    perror(calling_function);
+    exit(EXIT_FAILURE);
+  }
+  return ret;
+}
+
+bool precise_read(int fd, char* buffer, int size) {
+   int lu, i = 0;
+   while (i < size && (lu = read(fd, buffer, size - i)) > 0) {
+      i += lu;
+   }
+   
+   if (lu < 0) {
+      perror("read()");
+   }
+   
+   return lu > 0;
+}
+
 void log_query(query_result_t* result) {
     char buffer[512];
     if (result->status == QUERY_SUCCESS) {
